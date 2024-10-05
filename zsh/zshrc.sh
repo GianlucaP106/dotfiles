226c0118163db53f946bd2b9c6420433f5eb4a01
Gianluca Piccirillo <gianluca.piccirillo@mail.mcgill.ca>
2024-10-05 16:20:15 -0400

    fixed prompt by surrounding arrow char

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -163,13 +163,8 @@ alias v.="v ."
 alias m="mynav"
 alias tm="tmux"
 
-PROMPT=$'%{\e[1m%}%{%F{#FFD28F}%}%T %{%f%F{#7db6ff}%}%3~%(0?. . %{%f%F{red}%}%? )%{%f%F{#c7c7c7}%}$(git_prompt_info)%{%f%}
-::: %{%F{#b3f971}%}»%{%f\e[0m%} '
-
 # shell prompt
-# PROMPT=$'\n \e[1m%F{#7db6ff}%~%f %F{#cad6f5}$(git_prompt_info)%f \n %F{#b3f971}→%f\e[0m '
-# had to wrap color codes in %{}
-# PROMPT=$'%{\e[1m%}%{%F{#7db6ff}%}%~%{%f%} %{%F{#cad6f5}%}$(git_prompt_info)%{%f%}\n%{%F{#b3f971}%}→%{%f%}%{\e[0m%} '
+PROMPT=$'%{\e[1m%}%{%F{#FFD28F}%}%T %{%f%F{#7db6ff}%}%3~%(0?. . %{%f%F{red}%}%? )%{%f%F{#c7c7c7}%}$(git_prompt_info)%{%f%}\n::: %{%F{#b3f971}%}%{%G»%}%{%f\e[0m%} '
 
 # mypass
 export PATH="$PATH:$HOME/.mypass"
